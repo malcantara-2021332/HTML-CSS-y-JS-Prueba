ef9fbf99e054083838f7fd30827a3fdfe68ddd1c
Marcos Alcantara <malcantara-2021332@kinal.edu.gt>
2023-01-18 14:49:14 -0600

    Primer commit v1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mi primer Repositorio en internet
+
+Saludos a Todos gente :D este sera un pequeño ejemplo del 
+uso de HTML CSS y JS en un ejemplo de suma de dos numeros
+y la suma de tales.
+
+:D
---
+++ index.js
@@ -0,0 +1,7 @@
+function sumar(){
+    let valor1 = document.getElementById("valor1").value;
+    let valor2 = document.getElementById("valor2").value;
+
+    let suma = parseInt(valor1) + parseInt(valor2);
+    console.log(suma);
+}
